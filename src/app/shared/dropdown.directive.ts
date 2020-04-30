c6ea4b10053cca38d19aaa7bff3a7a38237bcf31
Tanya Robertson <tanya@tanyas-imac.lan>
2020-04-30 13:50:03 -0400

    dropdown directive

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -10,3 +10,16 @@ export class DropdownDirective {
     this.isOpen = !this.isOpen;
   }
 }
+// to make it close on document click anywhere:
+// import { Directive, ElementRef, HostBinding, HostListener } from '@angular/core';
+
+// @Directive({
+//   selector: '[appDropdown]'
+// })
+// export class DropdownDirective {
+//   @HostBinding('class.open') isOpen = false;
+//   @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+//     this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
+//   }
+//   constructor(private elRef: ElementRef) { }
+// }
