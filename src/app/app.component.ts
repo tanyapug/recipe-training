731f65ca3dbd9422ed0c5fa232b47d8bdf04aa2a
Tanya Robertson <tanya@tanyas-imac.lan>
2020-05-04 12:50:06 -0400

    starting to inject services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { ShoppingListService } from './shopping-list/shopping-list.service';
+import { RecipesService } from './recipes/recipes.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [ShoppingListService, RecipesService]
 })
 export class AppComponent {
   title = "my-recipe-app";
---
+++ src/app/shopping-list/shopping-list.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShoppingListService {
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+  ];
+
+
+  onIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
+}
